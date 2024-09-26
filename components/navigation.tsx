1c8899b696157e8140afdbad88733d6247832ac6
steven.hagene <steven.hagene@gmail.com>
2024-09-25 21:54:41 -0400

    Twaked the navbar

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,26 +3,26 @@ import Image from "next/image"
 
 export function Navigation() {
   return (
-    <header className="px-4 lg:px-6 h-14 flex items-center bg-slate-50">
+    <header className="px-4 lg:px-6 h-20 flex items-center bg-slate-50">
       <Link className="flex items-center justify-center" href="/">
-        <Image
+        {/* <Image
           src="/knowledgesphere.png"
           alt="KnowledgeSphere Logo"
-          width={32}
-          height={32}
-          className="h-6 w-6"
+          width={48}
+          height={48}
+          className="h-12 w-12"
           priority
-        />
-        <span className="ml-2 text-lg font-semibold">KnowledgeSphere</span>
+        /> */}
+        <span className="ml-3 text-2xl font-semibold">KnowledgeSphere</span>
       </Link>
-      <nav className="ml-auto flex gap-4 sm:gap-6">
-        <Link className="text-sm font-medium hover:underline underline-offset-4" href="/">
+      <nav className="ml-auto flex gap-6 sm:gap-8">
+        <Link className="text-base font-medium hover:underline underline-offset-4" href="/">
           Home
         </Link>
-        <Link className="text-sm font-medium hover:underline underline-offset-4" href="/create">
+        <Link className="text-base font-medium hover:underline underline-offset-4" href="/create">
           Create
         </Link>
-        <Link className="text-sm font-medium hover:underline underline-offset-4" href="/saved">
+        <Link className="text-base font-medium hover:underline underline-offset-4" href="/saved">
           Saved
         </Link>
       </nav>
